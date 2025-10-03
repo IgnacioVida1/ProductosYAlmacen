5f04ecd73159a3d5067e407be1d516870df49b00
Ignacio Vidal <ignacio.vidal@utec.edu.pe>
2025-10-03 02:56:33 -0500

    borrar pedidosclientes

--- routes/PruebaEntregaRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const PruebaController = require("../controllers/PruebaEntregaController");
-
-router.post("/", PruebaController.registrarEntrega);
-router.patch("/:id/validar", PruebaController.validarEntrega);
-
-module.exports = router;
